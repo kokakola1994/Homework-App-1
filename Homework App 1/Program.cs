34e3590decd2879600b4e173a574125784067dd3
Mykola Klymchuk <m.klymchuk@student.po.edu.pl>
2020-10-09 02:02:14 +0200

    0.01

--- Homework App 1/Program.cs
+++ Homework App 1/Program.cs
@@ -10,6 +10,20 @@ namespace Homework_App_1
     {
         static void Main(string[] args)
         {
+            int[,] myArr = new int[20, 40];
+
+            Random ran = new Random();
+
+            for (int i = 0; i < 20; i++)
+            {
+                for (int j = 0; j < 40; j++)
+                {
+                    myArr[i, j] = ran.Next(1, 15);
+                    Console.Write("{0}\t", myArr[i, j]);
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
         }
     }
 }
